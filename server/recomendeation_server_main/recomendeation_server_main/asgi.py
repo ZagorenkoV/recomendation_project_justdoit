9395f78667109cccc69c55eb482b9153085f3529
Viktor Zagorenko <vzagorenko@bk.ru>
2023-05-19 11:57:28 +0300

    initial commit

---
+++ server/recomendeation_server_main/recomendeation_server_main/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for recomendeation_server_main project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recomendeation_server_main.settings')
+
+application = get_asgi_application()
