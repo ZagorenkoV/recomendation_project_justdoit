55d4ccf1627594a449d917e0d15d819081ddc26f
igor_mnz <ib.monzin@gmail.com>
2023-05-28 23:27:58 +0300

    front: some fixes

--- client/src/mock/recommendedEvents.ts
+++ client/src/mock/recommendedEvents.ts
@@ -6,44 +6,52 @@ export interface IRecommendedEvents {
 }
 
 export const recommendedEvents: IRecommendedEvents[] = [
-	// {
-	// 	category: 'Категория 1',
-	// 	title: 'Заголовок 1',
-	// 	description: 'Описание 1 Описание 1 Описание 1Описание 1Описание 1 Описание 1'
-	// },
-	// {
-	// 	category: 'Категория 2',
-	// 	title: 'Заголовок 2',
-	// 	description: 'Описание 2 Описание 2 Описание 2Описание 2Описание 2 Описание 2'
-	// },
-	// {
-	// 	category: 'Категория 3',
-	// 	title: 'Заголовок 3',
-	// 	description: 'Описание 3 Описание 3 Описание 3Описание 3Описание 3 Описание 3'
-	// },
-	// {
-	// 	category: 'Категория 4',
-	// 	title: 'Заголовок 4',
-	// 	description: 'Описание 4 Описание 4 Описание 4Описание 4Описание 4 Описание 4'
-	// },
-	// {
-	// 	category: 'Категория 1',
-	// 	title: 'Заголовок 1',
-	// 	description: 'Описание 1 Описание 1 Описание 1Описание 1Описание 1 Описание 1'
-	// },
-	// {
-	// 	category: 'Категория 2',
-	// 	title: 'Заголовок 2',
-	// 	description: 'Описание 2 Описание 2 Описание 2Описание 2Описание 2 Описание 2'
-	// },
-	// {
-	// 	category: 'Категория 3',
-	// 	title: 'Заголовок 3',
-	// 	description: 'Описание 3 Описание 3 Описание 3Описание 3Описание 3 Описание 3'
-	// },
-	// {
-	// 	category: 'Категория 4',
-	// 	title: 'Заголовок 4',
-	// 	description: 'Описание 4 Описание 4 Описание 4Описание 4Описание 4 Описание 4'
-	// }
+	{
+		category: 'Категория 1',
+		title: 'Заголовок 1',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	},
+	{
+		category: 'Категория 2',
+		title: 'Заголовок 2',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	},
+	{
+		category: 'Категория 3',
+		title: 'Заголовок 3',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	},
+	{
+		category: 'Категория 4',
+		title: 'Заголовок 4',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	},
+	{
+		category: 'Категория 5',
+		title: 'Заголовок 5',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	},
+	{
+		category: 'Категория 6',
+		title: 'Заголовок 6',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	},
+	{
+		category: 'Категория 7',
+		title: 'Заголовок 7',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	},
+	{
+		category: 'Категория 8',
+		title: 'Заголовок 8',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		address: 'город Москва, Люблинская улица, дом 125А, строение 1'
+	}
 ]
